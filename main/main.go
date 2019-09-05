34aa9a2095d93c7b48fb22fe01d4ab923cce641b
Rajesh Jonnalagadda <rajesh.jonnalagadda@hitachiconsulting.com>
2019-09-05 19:41:25 +0530

    Adding first utility

---
+++ main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/vlnrajesh/golang-examples/utils"
+)
+
+func main() {
+	fmt.Println("Hello World now working\n")
+	fmt.Println(utils.Reverse("olleH Hello"))
+}
